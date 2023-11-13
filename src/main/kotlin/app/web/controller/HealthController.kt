1c6a7f3a1dbc8efaf9c19d51c8f81c8669b2d4ca
xodbar <xodbar@protonmail.com>
2023-11-13 13:31:44 +0600

    update /health

--- src/main/kotlin/app/web/controller/HealthController.kt
+++ src/main/kotlin/app/web/controller/HealthController.kt
@@ -1,16 +1,29 @@
 package app.web.controller
 
 import app.core.utils.getCurrentAlmatyLocalDateTime
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
+import java.io.Serializable
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/health")
-class HealthController {
+class HealthController(
+    @Value("\${app.version}") private val apiVersion: String
+) {
 
     @GetMapping
     @ResponseBody
-    fun health() = getCurrentAlmatyLocalDateTime()
+    fun health() = HealthResponse(
+        apiVersion = apiVersion,
+        timestamp = getCurrentAlmatyLocalDateTime()
+    )
 }
+
+data class HealthResponse(
+    val apiVersion: String,
+    val timestamp: LocalDateTime
+) : Serializable
